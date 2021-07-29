2f5f18b673b15f8b9b680a6540b8f244b13798fa
vic-meateater <victor.myasoedov@gmail.com>
2021-07-29 14:49:02 +0500

    AnimationToRagDoll.cs
    Adding AnimationToRagDoll.cs script. Then press G key, skeleton animation is disabled and the character drop -down to earth.

---
+++ Unity3D/Assets/Scripts/AnimationToRagDoll.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AnimationToRagDoll : MonoBehaviour
+{
+    private Rigidbody[] _rbRagDoll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rbRagDoll = GetComponentsInChildren<Rigidbody>();
+        foreach(Rigidbody rb in _rbRagDoll)
+        {
+            rb.isKinematic = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) RagdollActivate();   
+    }
+
+    private void RagdollActivate()
+    {
+        foreach (Rigidbody rb in _rbRagDoll) rb.isKinematic = false;
+        GetComponent<Animator>().enabled = false;
+    }
+}
