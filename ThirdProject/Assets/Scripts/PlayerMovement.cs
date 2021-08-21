f61c9d5aa46c94bb3b6b0b50e08353754711ab7f
vic-meateater <victor.myasoedov@gmail.com>
2021-08-21 18:08:12 +0500

    adding namespace
    namespace BananaMan

--- ThirdProject/Assets/Scripts/PlayerMovement.cs
+++ ThirdProject/Assets/Scripts/PlayerMovement.cs
@@ -1,61 +1,64 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace BananaMan
 {
-    [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private float _speed;
-    private int _animantorHashX;
-    private int _animantorHashZ;
-
-    Animator _animator;
-
-
-
-    //void Awake() => _animator = GetComponent<Animator>();
-    private void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        _animator = GetComponentInChildren<Animator>();
-        _animantorHashX = Animator.StringToHash("VelocityX");
-        _animantorHashZ = Animator.StringToHash("VelocityZ");
+        [SerializeField] private LayerMask _layerMask;
+        [SerializeField] private float _speed;
+        private int _animantorHashX;
+        private int _animantorHashZ;
 
-    }
-    private void Update()
-    {
-        MovePlayer();
-        AimTowardMouse();
-    }
-
-    private void MovePlayer()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        Animator _animator;
 
 
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
 
-        if(movement.magnitude > 0)
+        //void Awake() => _animator = GetComponent<Animator>();
+        private void Awake()
         {
-            movement.Normalize();
-            movement *= _speed * Time.deltaTime;
-            transform.Translate(movement, Space.World);
+            _animator = GetComponentInChildren<Animator>();
+            _animantorHashX = Animator.StringToHash("VelocityX");
+            _animantorHashZ = Animator.StringToHash("VelocityZ");
+
+        }
+        private void Update()
+        {
+            MovePlayer();
+            AimTowardMouse();
         }
 
-        float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
-        float velocityX = Vector3.Dot(movement.normalized, transform.right);
-
-        _animator.SetFloat(_animantorHashZ, velocityZ, 0.1f, Time.deltaTime);
-        _animator.SetFloat(_animantorHashX, velocityX, 0.1f, Time.deltaTime);
-    }
-
-    private void AimTowardMouse()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
+        private void MovePlayer()
         {
-            var _direction = hit.point - transform.position;
-            _direction.y = 0f;
-            _direction.Normalize();
-            transform.forward = _direction;
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+
+            Vector3 movement = new Vector3(moveX, 0f, moveZ);
+
+            if(movement.magnitude > 0)
+            {
+                movement.Normalize();
+                movement *= _speed * Time.deltaTime;
+                transform.Translate(movement, Space.World);
+            }
+
+            float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
+            float velocityX = Vector3.Dot(movement.normalized, transform.right);
+
+            _animator.SetFloat(_animantorHashZ, velocityZ, 0.1f, Time.deltaTime);
+            _animator.SetFloat(_animantorHashX, velocityX, 0.1f, Time.deltaTime);
+        }
+
+        private void AimTowardMouse()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out var hit, Mathf.Infinity, _layerMask))
+            {
+                var _direction = hit.point - transform.position;
+                _direction.y = 0f;
+                _direction.Normalize();
+                transform.forward = _direction;
+            }
         }
     }
 }
