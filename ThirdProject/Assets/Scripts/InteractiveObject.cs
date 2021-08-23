4864738ed809d120606763658a2958a467ef6ba4
vic-meateater <victor.myasoedov@gmail.com>
2021-08-23 16:57:12 +0500

    some todo

--- ThirdProject/Assets/Scripts/InteractiveObject.cs
+++ ThirdProject/Assets/Scripts/InteractiveObject.cs
@@ -2,8 +2,25 @@ using System;
 using UnityEngine;
 namespace BananaMan
 {
-    public abstract class InteractiveObject:MonoBehaviour
+    public abstract class InteractiveObject:MonoBehaviour, IComparable<InteractiveObject>
     {
         protected abstract void Interaction();
+
+        public bool IsInteractable { get; } = true;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!IsInteractable || !other.CompareTag("Player"))
+            {
+                return;
+            }
+            Debug.Log("Enter to Trigger");
+            Destroy(gameObject);
+        }
+
+        public int CompareTo(InteractiveObject other)
+        {
+            return name.CompareTo(other.name);
+        }
     }
 }
