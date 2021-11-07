d4119da47f1371df11c1a69818fc57163b7f3c83
vic-meateater <victor.myasoedov@gmail.com>
2021-11-07 11:13:00 +0500

    Add Game Starter

---
+++ HW2-Asteroids/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class GameStarter : MonoBehaviour
+    {
+        private void Start()
+        {
+            Enemy.CreateAsteroidEnemy(new Health(100f, 100f));
+        }
+    }
+}
