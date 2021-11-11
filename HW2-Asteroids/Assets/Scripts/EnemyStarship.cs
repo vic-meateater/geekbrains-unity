5f64c6943300d112dc75bae2cba3ffeb80f57f50
vic-meateater <victor.myasoedov@gmail.com>
2021-11-11 12:08:53 +0500

    Start Enemy logic

--- HW2-Asteroids/Assets/Scripts/EnemyStarship.cs
+++ HW2-Asteroids/Assets/Scripts/EnemyStarship.cs
@@ -1,7 +1,47 @@
-﻿namespace Asteroids
+﻿using UnityEngine;
+
+namespace Asteroids
 {
-    internal sealed class EnemyStarship : Enemy
+    internal sealed class EnemyStarship : Enemy, IRotation, IShoot
     {
-        
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private float _damage;
+        private Ship _ship;
+
+        private void Awake()
+        {
+            var shipRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+            var moveRigidbody = new MoveRigidbody(shipRigidbody2D, _speed);
+            var rotation = new RotationShip(transform);
+            var shooting = new ShootingShip(_barrel);
+            _ship = new Ship(moveRigidbody, rotation, shooting);
+        }
+
+        public override void DependencyInjectHealth(Health hp)
+        {
+            Health = hp;
+        }
+
+        public override void Move(float horizontal, float vertical, float deltaTime)
+        {
+            _ship.Move(horizontal, vertical, deltaTime);
+        }
+
+        public void Rotation(Vector3 direction)
+        {
+            _ship.Rotation(direction);
+        }
+
+        public void Shoot()
+        {
+            _ship.Shoot();
+        }
+
+        protected override void Interaction()
+        {
+            Player.Health -= _damage;
+            Health.ChangeCurrentHealth(Health.Current - _damage);
+        }
     }
 }
---
+++ HW2-Asteroids/Assets/Scripts/MoveRigidbody.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    internal class MoveRigidbody
+    {
+        private readonly Rigidbody2D _shipRigidbody2D;
+        private Vector3 _move;
+        public float Speed { get; protected set; }
+
+        public MoveRigidbody(Rigidbody2D shipRigidbody2D, float speed)
+        {
+            _shipRigidbody2D = shipRigidbody2D;
+            Speed = speed;
+        }
+
+        public void Move(float horizontal, float vertical, float deltaTime)
+        {
+            var speed = deltaTime * Speed;
+            _move.Set(horizontal * speed, vertical * speed, 0.0f);
+            _shipRigidbody2D.AddForce(_move);
+        }
+    }
+}
